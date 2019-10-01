a536f5b4a2d81b4926eb2e48f677c0ade62b7b13
Djupper <Djupper@mail.ru>
2019-10-01 14:00:37 +0700

    First blood!

---
+++ 30-09/script.js
@@ -0,0 +1,15 @@
+'use strict';
+a = 5;
+console.log(a);
+let inct =10,
+decr = 10;
+
+
+
+console.log(inct++);
+console.log(decr--);
+
+let  isChecked = true;
+let isClose = false ;
+
+console.log(isChecked && !isClose);
